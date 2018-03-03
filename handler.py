ca00a5addb98bb7c6bf923d9ec9492e92dad86cc
Alexey Kaytaz <alexey@kaytaz.ru>
2018-03-03 10:11:50 +0100

    masser function templates

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import os
 import sys
 import logging
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, "./vendored"))
@@ -13,6 +13,27 @@ import requests
 TOKEN = os.environ['TELEGRAM_TOKEN']
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
+def check_location(lat, long):
+    data = {}
+    data['status'] = 'ok'
+    data['zone_id'] = '2429809'
+    data['message'] = 'first check-in'
+    return data
+
+def zone_info(zone_id):
+    data = {}
+    data['zone_id'] = zone_id
+    data['open_status'] = 'ok'
+    data['start_time'] = '5/12/2016 8:50'
+    data['end_time'] = '5/12/2018 8:50'
+    data['bonus'] = 5000 #satoshi
+    data['interval'] = 300 #seconds
+    return data
+
+def reply(response, chat_id):
+        data = {"text": response.encode("utf8"), "chat_id": chat_id}
+        url = BASE_URL + "/sendMessage"
+        requests.post(url, data)
 
 def hello(event, context):
     try:
@@ -30,18 +51,20 @@ def hello(event, context):
                 response = "Hello {}".format(first_name)
 
         if "location" in data["message"]:
-            response = "Privet Likusa! :-)p"
-            #response = "latitude:{} longitude:{}".format(data["location"]["latitude"], data["location"]["longitude"])
-            logger.info(data)
+            location = check_location(data["message"]["location"]["latitude"],
+                                          data["message"]["location"]["longitude"])
+            if location['status'] == 'ok':
+                zone = zone_info(location['zone_id'])
+                response = str(zone)
+                logger.debug(zone)
+                logger.debug(response)
 
-        data = {"text": response.encode("utf8"), "chat_id": chat_id}
-        url = BASE_URL + "/sendMessage"
-        logger.info("post_url:{}".format(url))
-        logger.info("post_data:{}".format(data))
-        requests.post(url, data)
+        reply(response, chat_id)
+
+
 
     except Exception as e:
-        print("Exeption")
-        print(e)
+        logger.error("Exeption")
+        logger.error(e)
 
     return {"statusCode": 200}
